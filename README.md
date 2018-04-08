3c7c8c30e97740a3673f1d6854ec715923d9b9d8
星翼 <853974536@qq.com>
2018-04-08 23:35:43 +0800

    Init2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RotatingCube
 基于FunCode的一个游戏。
+
+（虽然只用到了FunCode建的窗口）
---
+++ SourceCode/Header/stdafx.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <cmath>
+
+#include <optional>
+#include <memory>
+#include <stack>
+#include <ostream>
+#include <string>
+#include <stack>
+#include <memory>
+#include <vector>
+#include <map>
+#include <set>
+#include <array>
+#include <stdexcept>
+#include <queue>
+#include <atomic>
+#include <functional>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <mutex> 
+#include <deque>
