75df4df14f3157c65a3fd7ec7157a202b36b971c
SengJik <Administrator@3303-48>
2018-05-30 16:14:42 +0800

    dEMO

--- SourceCode/VCProject2010/VCProject2010/BlueScreen.h
+++ SourceCode/VCProject2010/VCProject2010/BlueScreen.h
@@ -1,18 +1,22 @@
 #pragma once
 #include "Device.h"
 #include "GameObject.h"
-
+#include "PostEffect.h"
+#include "Shaders.h"
 namespace Game
 {
 	namespace EngineTests
 	{
 		class BlueScreen : public Engine::GameObject
 		{
+		private:
+			Engine::Rendering::PostEffect pe_;
 		public:
+			inline BlueScreen() : pe_{ "ColorOnly" } {}
 			virtual void Draw() const
 			{
 				const float blue[4] = { 0,0,1,1 };
-				auto& device = Engine::Rendering::GetDevice();
+				auto& device = Engine::GetDevice();
 				device.Context().ClearRenderTargetView(&device.RenderTargetView(), blue);
 			}
 		};
