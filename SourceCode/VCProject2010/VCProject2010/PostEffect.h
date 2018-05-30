2bc9355474bf7cbe7d107cb97f366ab8d04762de
SengJik <Administrator@3303-48>
2018-05-30 15:51:48 +0800

    Post Effect

---
+++ SourceCode/VCProject2010/VCProject2010/PostEffect.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "VertexIn.h"
+
+namespace Engine
+{
+	namespace Rendering
+	{
+		class PostEffect
+		{
+		private:
+			ComPtr<ID3D11PixelShader> peps_;
+			ComPtr<ID3D11Buffer> pscb_,vb_,vscb_;
+		public:
+			PostEffect(const char* peps);
+			PostEffect(const PostEffect&) = delete;
+			const PostEffect& operator =(const PostEffect&) = delete;
+
+			void SetConstantBuffer(const ComPtr<ID3D11Buffer>&);
+			void Draw() const;
+		};
+	}
+}
